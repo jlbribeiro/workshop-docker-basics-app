4bfcf54c882d2bc7ef2ac695dd042419f41e6406
José Ribeiro <hello@jlbribeiro.com>
2016-04-09 02:22:29 +0100

    Two containers talking to each other!
    A basic visit counter using Redis.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
+import redis
 from flask import Flask
+
 app = Flask(__name__)
+r = redis.StrictRedis(host='redis', port=6379, db=0)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    n_visits = r.incr('visits')
+    return "Hello, visitor {0}!".format(n_visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
